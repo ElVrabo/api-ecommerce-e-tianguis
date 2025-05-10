84e5be4d2f3bc9b91f3dce3afe3567d670ccd907
Brandonmkmk <brandonxd611@gmail.com>
2025-05-10 13:14:35 -0600

    fix(products): fix getProducts controller

--- src/routes/favoriteProducts.routes.js
+++ src/routes/favoriteProducts.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validatorToken.js";
-import { getFavoriteProducts, saveFavoriteProducts } from "../controllers/favoriteProducts.controller.js";
+import { deleteFavoriteProduct, getFavoriteProducts, saveFavoriteProducts } from "../controllers/favoriteProducts.controller.js";
 
 
 const favoriteProductsRoutes = Router()
 
 favoriteProductsRoutes.get('/favoriteProducts',authRequired,getFavoriteProducts)
 favoriteProductsRoutes.post('/favoriteProducts',authRequired,saveFavoriteProducts)
-favoriteProductsRoutes.delete('/favoriteProducts/:id',saveFavoriteProducts)
+favoriteProductsRoutes.delete('/favoriteProducts/:id',deleteFavoriteProduct)
 
 export default favoriteProductsRoutes
